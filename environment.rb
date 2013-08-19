8d685143e3e343492ab4e5380eddf2a3b3b4ae09
Roberto Soares <roberto.tech@gmail.com>
2013-08-19 18:35:43 -0300

    mongodb prod setup

--- environment.rb
+++ environment.rb
@@ -2,6 +2,8 @@ Bundler.setup :default, (ENV['RACK_ENV'] || 'development')
 
 require 'mongo_mapper'
 
-MongoMapper.setup({ENV['RACK_ENV'] => {'uri' => 'mongodb://localhost/foy'}}, ENV['RACK_ENV'])
+mongo_uri = ENV['MONGO_URI'] || 'mongodb://localhost/foy'
+
+MongoMapper.setup({ENV['RACK_ENV'] => {'uri' => mongo_uri}}, ENV['RACK_ENV'])
 
 require_relative 'lib/foy_api'
