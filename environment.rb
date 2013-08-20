99480385e4eb9addb5a1d09caad4b347ff3de0f6
Roberto Soares <roberto.tech@gmail.com>
2013-08-19 23:09:06 -0300

    mongodb uri

--- environment.rb
+++ environment.rb
@@ -2,7 +2,12 @@ Bundler.setup :default, (ENV['RACK_ENV'] || 'development')
 
 require 'mongo_mapper'
 
-mongo_uri = ENV['MONGO_URI'] || 'mongodb://localhost/foy'
+mongo_uri = if ENV['MONGO_URI']
+  #mongodb://[username:password@]host1[:port1]
+  "mongodb://#{ENV['MONGO_USER']}:#{ENV['MONGO_PASSWORD']}@#{ENV['MONGO_URI']}"
+else
+  'mongodb://localhost/foy'
+end
 
 MongoMapper.setup({ENV['RACK_ENV'] => {'uri' => mongo_uri}}, ENV['RACK_ENV'])
 
