446873a99016e6376df00248cc1022631e61dcc6
Roberto Soares <roberto.tech@gmail.com>
2014-01-28 14:01:07 -0200

    mongo database for each environment

--- environment.rb
+++ environment.rb
@@ -1,4 +1,10 @@
-Bundler.setup :default, (ENV['RACK_ENV'] || 'development')
+require 'rubygems'
+require 'bundler'
+
+ENV['RACK_ENV'] ||= 'development'
+
+Bundler.setup :default, ENV['RACK_ENV'].to_sym
+Bundler.require :default, ENV['RACK_ENV'].to_sym
 
 require 'mongo_mapper'
 
@@ -6,7 +12,7 @@ mongo_uri = if ENV['MONGO_URI']
   #mongodb://[username:password@]host1[:port1]
   "mongodb://#{ENV['MONGO_USER']}:#{ENV['MONGO_PASSWORD']}@#{ENV['MONGO_URI']}/#{ENV['MONGO_DATABASE_NAME']}"
 else
-  'mongodb://localhost/foy'
+  "mongodb://localhost/foy_#{ENV['RACK_ENV']}"
 end
 
 MongoMapper.setup({ENV['RACK_ENV'] => {'uri' => mongo_uri}}, ENV['RACK_ENV'])
