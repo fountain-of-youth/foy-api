72ee0dcd03bf7b4405b802de39e84d6a6625a171
Roberto Soares <roberto.tech@gmail.com>
2014-01-31 14:05:27 -0200

    add service to update packages: /packages/:system.json

--- lib/foy_api.rb
+++ lib/foy_api.rb
@@ -17,6 +17,23 @@ module Foy
             error! 'Not Found', 404
           end
         end
+
+        desc "Update version of packages"
+        params do
+          requires :packages,   type: Array,  desc: "List of packages"
+        end
+        put do
+          begin
+            package_system = PackageSystem.find_by_name!(params[:system])
+            params[:packages].each do |param_package|
+              package = package_system.packages.find_or_create_by_name(param_package[:name])
+              package.version = param_package[:version]
+              package.save!
+            end
+          rescue MongoMapper::DocumentNotFound
+            error! 'Not Found', 404
+          end
+        end
       end
     end
 
--- spec/lib/foy_api_spec.rb
+++ spec/lib/foy_api_spec.rb
@@ -12,22 +12,22 @@ describe Foy::API do
       FactoryGirl.create(:package_system, name: 'pip')
     end
 
-    let!(:other_packages) do
-      FactoryGirl.create_list(:package, 3)
-    end
-
-    let!(:packages) do
-      FactoryGirl.create_list(:package, 3, package_system: system)
-    end
-
     describe "GET /v1/packages/:system.json" do
+      let!(:other_packages) do
+        FactoryGirl.create_list(:package, 3)
+      end
+
+      let!(:packages) do
+        FactoryGirl.create_list(:package, 3, package_system: system)
+      end
+
       it "returns all packages of a system" do
-        get '/v1/packages/pip.json'
+        get "/v1/packages/pip.json"
         last_response.body.should == packages.to_json
       end
 
       it "returns only name and version for each package" do
-        get '/v1/packages/pip.json'
+        get "/v1/packages/pip.json"
         returned_packages = JSON.parse(last_response.body)
         returned_packages.each do |pkg|
           expect(pkg.keys).to be == ['name', 'version']
@@ -42,6 +42,61 @@ describe Foy::API do
       end
     end
 
+    describe "PUT /v1/packages/:system.json" do
+      let!(:other_packages) do
+        FactoryGirl.create_list(:package, 3, package_system: system)
+      end
+
+      context "package system not found" do
+        let(:data) do
+          {packages: [{name: 'rest-client', version: '1.0.1'}]}
+        end
+
+        it "returns 404" do
+          put '/v1/packages/xyz.json', data
+          last_response.status.should == 404
+        end
+      end
+
+      context "new packages" do
+        let(:data) do
+          {packages: [{name: 'rest-client', version: '1.0.1'}, {name: 'rspec', version: '2.0.0'}]}
+        end
+
+        it "creates nonexistent packages" do
+          put "/v1/packages/#{system.name}.json", data
+          expect(system.packages.first(name: 'rest-client')).to_not be_nil
+          expect(system.packages.first(name: 'rspec')).to_not be_nil
+          expect(system.packages.count).to be ==(5)
+        end
+      end
+
+      context "updating packages" do
+        let!(:current_packages) do
+          FactoryGirl.create_list(:package, 3, package_system: system)
+        end
+
+        let(:package) do
+          other_packages.first
+        end
+
+        let(:data) do
+          {packages: [{name: package.name, version: package.version}]}
+        end
+
+        let(:put_data) do
+          put "/v1/packages/#{system.name}.json", data
+        end
+
+        it "doesn't udpate number of packages" do
+          expect{put_data}.to_not change{Package.count}.from(3)
+        end
+    
+        it "doesn't updates package" do
+          expect{put_data}.to_not change{Package.all}
+        end
+      end
+    end
   end
 
   describe "projects" do
@@ -113,29 +168,30 @@ describe Foy::API do
     end
   end
 
-  describe "packages" do
+  describe "projects/packages" do
     let!(:project)          { FactoryGirl.create(:project) }
     let!(:package_system)   { FactoryGirl.create(:package_system) }
     let!(:current_packages) { FactoryGirl.create_list(:package, 2, package_system: package_system) }
     let(:put_data)          { put "/v1/projects/#{project.id}/packages", data }
 
     describe "PUT /v1/projects/:id/packages" do
+
       context "new packages" do
         let(:data) do
           {system: package_system.name, packages: [{name: 'rest-client', version: '1.0.1'}, {name: 'rspec', version: '2.0.0'}]}
-        end
-
-        it "creates nonexistent packages" do
-          put_data
-          expect(Package.first(name: 'rest-client')).to_not be_nil
-          expect(Package.first(name: 'rspec')).to_not be_nil
-          expect(Package.count).to be_eql(4)
         end
 
         it "associates project with its packages" do
           put_data
           expect(project.packages).to include(Package.first(name: 'rest-client'))
           expect(project.packages).to include(Package.first(name: 'rspec'))
+        end
+
+        it "creates nonexistent packages" do
+          put_data
+          expect(package_system.packages.first(name: 'rest-client')).to_not be_nil
+          expect(package_system.packages.first(name: 'rspec')).to_not be_nil
+          expect(package_system.packages.count).to be ==(4)
         end
       end
 
@@ -147,7 +203,7 @@ describe Foy::API do
         it "updates current project packages" do
           expect{put_data}.to change{project.reload; project.packages}.from([]).to(current_packages)
         end
-        
+    
         it "doesn't updates package" do
           expect{put_data}.to_not change{Package.all}
         end
