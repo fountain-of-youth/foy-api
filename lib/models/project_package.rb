ab3adae7efae9fad570bb869762cb567bfa0305b
Roberto Soares <roberto.tech@gmail.com>
2014-03-11 18:17:55 -0300

    project/packages returning last_version and improved status

--- lib/models/project_package.rb
+++ lib/models/project_package.rb
@@ -9,11 +9,21 @@ class ProjectPackage
     Gem::Version.new(version) >= Gem::Version.new(self.package.version) rescue nil
   end
 
+  def status
+    return nil if last_version.nil?
+    updated? ? 'updated' : 'outdated'
+  end
+
+  def last_version
+    package.version
+  end
+
   def as_json(options = {})
     super(options).tap do |attrs|
-      attrs[:status]  = updated? ? 'updated' : 'outdated'
+      attrs[:status]  = status
       attrs[:system]  = package.package_system.name
       attrs[:name]    = package.name
+      attrs[:last_version] = package.version
     end
   end
 end
